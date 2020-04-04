06e36cda95f4ae02dbf8851aa201e627964208a5
eduardo.balan <eduardo.balan@azi.com.br>
2020-04-03 22:39:04 -0400

    removendo item não utilizado

--- src/main/java/br/com/albus/comum/pessoa/PessoaController.java
+++ src/main/java/br/com/albus/comum/pessoa/PessoaController.java
@@ -13,9 +13,6 @@ import java.util.Objects;
 @Component
 public class PessoaController extends GenericController<Pessoa, PessoaRepository, Long> {
 
-    @Autowired
-    private PessoaRepository repository;
-
     @Override
     public Pessoa inserir(Pessoa entityNova) {
         throw new GenericRuntimeException("Vc deve utilizar o recurso /pessoa/fisica ou /pessoa/juridica", null);
