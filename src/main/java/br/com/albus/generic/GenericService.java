cdc2665bfab12823cf1b15fb4292f4deb8f49929
eduardo.balan <eduardo.balan@azi.com.br>
2020-05-11 17:42:15 -0400

    inserindo @CrossOrigin

--- src/main/java/br/com/albus/generic/GenericService.java
+++ src/main/java/br/com/albus/generic/GenericService.java
@@ -1,6 +1,7 @@
 package br.com.albus.generic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 import java.util.List;
 
 @MappedSuperclass
+@CrossOrigin
 public class GenericService<
         C extends GenericController<E, R, I>,
         E extends GenericEntity<I>,
@@ -28,26 +30,31 @@ public class GenericService<
     private R repository;
 
     @GetMapping
+    @CrossOrigin
     public List<E> buscarTodos() {
         return repository.findAll();
     }
 
     @GetMapping(path = "/{id}")
+    @CrossOrigin
     public Object buscarPorId(@PathVariable("id") I id) {
         return repository.findById(id);
     }
 
     @PostMapping
+    @CrossOrigin
     public E inserir(@RequestBody E entitySalvar) {
         return business.inserirComValidacao(entitySalvar);
     }
 
     @PutMapping(path = "/{id}")
+    @CrossOrigin
     public E editar(@RequestBody E entityUpdate, @PathVariable("id") I id) {
         return business.editarComValidacao(entityUpdate, id);
     }
 
     @DeleteMapping(path = "/{id}")
+    @CrossOrigin
     public void excluir(@PathVariable("id") I id) {
         business.excluir(id);
     }
